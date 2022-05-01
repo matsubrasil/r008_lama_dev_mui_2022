e9d277f4e92efddd0ddac0ad5c6877a9acc6d05b
matsubrasil <matsuboy@outlook.com>
2022-05-01 13:08:40 -0300

    feat(layout): implement sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,98 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+  Switch,
+} from '@mui/material';
+import {
+  AccountBox,
+  Article,
+  Group,
+  Home,
+  ModeNight,
+  Person,
+  Settings,
+  Storefront,
+} from '@mui/icons-material';
 
 export const Sidebar: React.FC = () => {
+  const ListItemIconCustom = styled(ListItemIcon)(({ theme }) => ({
+    minWidth: theme.spacing(4),
+  }));
+
   return (
-    <Box
-      bgcolor='skyblue'
-      flex={1}
-      p={2}
-      sx={{ display: { xs: 'none', sm: 'block' } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIconCustom>
+              <Home />
+            </ListItemIconCustom>
+            <ListItemText primary='Homepage' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <Article />
+            </ListItemIconCustom>
+            <ListItemText primary='Pages' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <Group />
+            </ListItemIconCustom>
+            <ListItemText primary='Groups' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <Storefront />
+            </ListItemIconCustom>
+            <ListItemText primary='Marketplace' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <Person />
+            </ListItemIconCustom>
+            <ListItemText primary='Friends' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <Settings />
+            </ListItemIconCustom>
+            <ListItemText primary='Settings' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <AccountBox />
+            </ListItemIconCustom>
+            <ListItemText primary='Profile' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIconCustom>
+              <ModeNight />
+            </ListItemIconCustom>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
